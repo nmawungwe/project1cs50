0a2f03c74fb69750157a7a1ce1bf26b67f5265e4
Nyasha Vision <nmawungwe@gmail.com>
2020-06-01 19:40:34 +0200

    created API

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, request, jsonify
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from config import DevelopmentConfig
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -17,13 +20,51 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+db = SQLAlchemy(app)
 
+from models import Book
 
 @app.route("/")
-def index():
-    return "Project 1: TODO"
+def hello():
+    return "Hello World!"
+
+@app.route("/add")
+def add_book():
+    name=request.args.get('name')
+    author=request.args.get('author')
+    published=request.args.get('published')
+    try:
+        book=Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return f"Book added. book id={book.id}"
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getall")
+def get_all():
+    try:
+        books=Book.query.all()
+        return  jsonify([e.serialize() for e in books])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book=Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+	    return(str(e))
+
+if __name__ == '__main__':
+    app.run()
 
 
 
