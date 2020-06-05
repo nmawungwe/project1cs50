3103c5ae8837ebb4b12a6dd8dde89eba426daa28
Nyasha Vision <nmawungwe@gmail.com>
2020-06-05 10:12:27 +0200

    added authentification logic

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from config import DevelopmentConfig
 from forms import LoginForm
+from flask_login import LoginManager, current_user, login_user, logout_user
+
 
 
 
@@ -28,8 +30,10 @@ Session(app)
 db = SQLAlchemy(app)
 
 # set up login LoginManager
+login = LoginManager(app)
 
 from models import Book, User, Review
+
 
 @app.route("/")
 @app.route("/index")
@@ -49,12 +53,25 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect({{ url_for('index') }})
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+ 
 @app.route("/add")
 def add_book():
     name=request.args.get('name')
--- models.py
+++ models.py
@@ -1,7 +1,10 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -11,7 +14,17 @@ class User(db.Model):
     reviews = db.relationship('Review', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return f'<User {self.username}>'  
+        return f'<User {self.username}>' 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password) 
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Book(db.Model):
     __tablename__ = 'book'
